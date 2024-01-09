52ba95bfbd088c9fb0820c6d79f01aeff1df57b6
Sarnai <D.Brusnikov@gmail.com>
2024-01-09 02:58:41 +0100

    feat: add ProductionStateTracker

---
+++ Assets/Scripts/Production/ProductionStateTracker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Production.Challenges;
+using UnityEngine;
+
+namespace Production
+{
+    public class ProductionStateTracker : MonoBehaviour
+    {
+        private int _criticalFailCount;
+        [SerializeField] private GeneralBase[] _generalChallenges;
+        
+        private void Start()
+        {
+            foreach (GeneralBase general in _generalChallenges)
+            {
+                general.GeneralFailed += AddCriticalFail;
+            }
+        }
+
+        public void OnDestroy()
+        {
+            foreach (GeneralBase general in _generalChallenges)
+            {
+                general.GeneralFailed -= AddCriticalFail;
+            }
+        }
+
+        private void AddCriticalFail()
+        {
+            if (_criticalFailCount < 3)
+            {
+                _criticalFailCount++;
+            }
+            else
+            {
+                FailProduction();
+            }
+        }
+
+        private void FailProduction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
