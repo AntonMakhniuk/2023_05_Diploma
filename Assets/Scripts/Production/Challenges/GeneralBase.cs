9b8cfc388036b4fc2927cd53ec9e16ec3b4ff5b7
Sarnai <D.Brusnikov@gmail.com>
2024-01-09 02:58:26 +0100

    feat: introduce GeneralBase

---
+++ Assets/Scripts/Production/Challenges/GeneralBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Production.Challenges
+{
+    public abstract class GeneralBase : MonoBehaviour
+    {
+        private void Update()
+        {
+            UpdateChallenge();
+        }
+
+        protected abstract void UpdateChallenge(); 
+     
+        public delegate void GeneralFailHandler();
+        public event GeneralFailHandler GeneralFailed;
+        
+        private void Fail()
+        {
+            GeneralFailed?.Invoke();
+            Reset();
+        }
+
+        protected abstract void Reset();
+    }
+}
