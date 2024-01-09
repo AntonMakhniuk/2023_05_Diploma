28ba684f2421b52384e0a7a9ae81f3512648377b
Oleksii <skorikalesha@gmail.com>
2024-01-09 14:04:07 +0100

    Update DrillBehaviour.cs

--- Assets/Scripts/DrillBehaviour.cs
+++ Assets/Scripts/DrillBehaviour.cs
@@ -2,6 +2,64 @@ using UnityEngine;
 
 public class DrillBehaviour : MonoBehaviour
 {
+    private Renderer drillRenderer;
+    private Collider drillCollider;
+
+    private void Start()
+    {
+        // Get the renderer and collider components
+        drillRenderer = GetComponent<Renderer>();
+        drillCollider = GetComponent<Collider>();
+
+        // Hide the drill by default
+        HideDrill();
+    }
+
+    private void Update()
+    {
+        // Toggle drill visibility on the "2" key
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ToggleDrillVisibility();
+        }
+    }
+
+    private void ToggleDrillVisibility()
+    {
+        // Toggle the visibility of the drill by enabling/disabling its renderer and collider
+        if (drillRenderer != null && drillCollider != null)
+        {
+            if (drillRenderer.enabled)
+            {
+                HideDrill();
+            }
+            else
+            {
+                ShowDrill();
+            }
+        }
+    }
+
+    private void HideDrill()
+    {
+        // Hide the drill by disabling its renderer and collider
+        if (drillRenderer != null && drillCollider != null)
+        {
+            drillRenderer.enabled = false;
+            drillCollider.enabled = false;
+        }
+    }
+
+    private void ShowDrill()
+    {
+        // Show the drill by enabling its renderer and collider
+        if (drillRenderer != null && drillCollider != null)
+        {
+            drillRenderer.enabled = true;
+            drillCollider.enabled = true;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if the trigger collider is an object tagged as "AsteroidPoint"
