2db09d4a4794e16bfeac644e6dc9127b2617b529
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2024-01-14 09:30:53 +0100

    Added TractorBeam
    Added tractor beam. There is spawner as empty object of ship. Pressing key 1 its spawns tractor beam. Tractor beam has Crosshair texture(can be added our later preferably 20x20), Its range and speed And its speed(I put 100 so it will show changes)

--- Assets/Scripts/Instruments/TractorBeam.cs
+++ Assets/Scripts/Instruments/TractorBeam.cs
@@ -1,58 +1,46 @@
-using System.Collections;
 using UnityEngine;
 
 public class TractorBeam : MonoBehaviour
 {
-    public GameObject tractorBeamPrefab; // The Tractor Beam prefab
-    public Texture2D crosshairTexture; // Crosshair texture
-    public float tractorBeamRange = 10f; // Effective range of the tractor beam
-    public float tractorSpeed = 5f; // Speed at which the tractor beam pulls objects
+    public Texture2D crosshairTexture;
+    public float tractorBeamRange = 10f;
+    public float tractorSpeed = 5f;
+    public float TractorBeamOffset { get; private set; }
 
-    private GameObject tractorBeamInstance; // Instance of the tractor beam
-    private bool isTractorBeamActive = false; // Flag to check if the tractor beam is active
+    private bool isTractorBeamActive = false;
+
+    void Start()
+    {
+        TractorBeamOffset = 0; // Set the default offset
+    }
 
     void Update()
     {
-        // Toggle Tractor Beam with the left mouse button (LMB)
-        if (Input.GetMouseButtonDown(0))
-        {
-            ToggleTractorBeam();
-        }
-
-        // Rotate the tractor beam along with the ship
         if (isTractorBeamActive)
         {
-            tractorBeamInstance.transform.rotation = transform.rotation;
+            UpdateTractorBeamPosition();
+            RotateTractorBeam();
         }
 
-        // Check for left mouse button input to pull objects
         if (Input.GetMouseButton(0) && isTractorBeamActive)
         {
             PullObject();
         }
 
-        // Check for right mouse button input to push objects
         if (Input.GetMouseButtonDown(1) && isTractorBeamActive)
         {
             PushObjects();
         }
     }
 
-    void ToggleTractorBeam()
+    void RotateTractorBeam()
     {
-        Debug.Log("Toggle Tractor Beam");
-        if (isTractorBeamActive)
-        {
-            // Deactivate Tractor Beam
-            Destroy(tractorBeamInstance);
-            isTractorBeamActive = false;
-        }
-        else
-        {
-            // Activate Tractor Beam
-            tractorBeamInstance = Instantiate(tractorBeamPrefab, transform.position - transform.up, Quaternion.identity);
-            isTractorBeamActive = true;
-        }
+        transform.rotation = transform.parent.rotation;
+    }
+
+    void UpdateTractorBeamPosition()
+    {
+        transform.position = transform.parent.position - transform.parent.up * TractorBeamOffset;
     }
 
     void PullObject()
@@ -60,35 +48,40 @@ public class TractorBeam : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, tractorBeamRange) && hit.collider.CompareTag("ResourceNode"))
+        if (Physics.Raycast(ray, out hit, tractorBeamRange))
         {
-            // Pull the object towards the ship
-            hit.rigidbody.AddForce((transform.position - hit.transform.position).normalized * tractorSpeed);
+            hit.rigidbody.AddForce((transform.parent.position - hit.transform.position).normalized * tractorSpeed);
         }
     }
 
     void PushObjects()
     {
-        // Create a force cone in front of the tractor beam
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, tractorBeamRange);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.parent.position, tractorBeamRange);
         foreach (Collider collider in hitColliders)
         {
-            if (collider.CompareTag("ResourceNode"))
-            {
-                // Push objects within the cone away from the ship
-                Vector3 forceDirection = (collider.transform.position - transform.position).normalized;
+            
+                Vector3 forceDirection = (collider.transform.position - transform.parent.position).normalized;
                 collider.GetComponent<Rigidbody>().AddForce(forceDirection * tractorSpeed, ForceMode.Impulse);
-            }
+            
         }
     }
 
     void OnGUI()
     {
-        // Display crosshair in the center of the screen
         if (isTractorBeamActive)
         {
             float size = 20f;
             GUI.DrawTexture(new Rect((Screen.width - size) / 2, (Screen.height - size) / 2, size, size), crosshairTexture);
         }
+    }
+
+    public void ActivateTractorBeam()
+    {
+        isTractorBeamActive = true;
+    }
+
+    public void DeactivateTractorBeam()
+    {
+        isTractorBeamActive = false;
     }
 }
--- Assets/Scripts/ShipInventory.cs
+++ Assets/Scripts/ShipInventory.cs
@@ -1,37 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipInventory : MonoBehaviour
+public class TractorBeamSpawner : MonoBehaviour
 {
-    public List<GameObject> instruments; // List of instruments in the ship's inventory
-    [SerializeField]private int currentInstrumentIndex = 0; // Index of the currently selected instrument
+    public GameObject tractorBeamPrefab;
+    public Transform shipTransform; // Assign the ship's transform in the Inspector
 
+    private TractorBeam tractorBeamInstance;
+    
     void Update()
     {
-        // Toggle instruments with number keys (1 to 9)
-        for (int i = 0; i <= instruments.Count; i++)
+        // Toggle Tractor Beam with the key (1)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-            {
-                SwitchInstrument(i);
-            }
+            ToggleTractorBeam();
         }
     }
 
-    void SwitchInstrument(int newIndex)
+    void ToggleTractorBeam()
     {
-        // Deactivate the current instrument
-        if (currentInstrumentIndex < instruments.Count)
+        if (tractorBeamInstance == null)
         {
-            instruments[currentInstrumentIndex].SetActive(false);
+            // Instantiate Tractor Beam and set shipTransform as the parent
+            tractorBeamInstance = Instantiate(tractorBeamPrefab, shipTransform).GetComponent<TractorBeam>();
+            tractorBeamInstance.transform.localPosition = Vector3.right * tractorBeamInstance.TractorBeamOffset; // Adjust offset to spawn horizontally
+            tractorBeamInstance.ActivateTractorBeam(); // Activate the tractor beam
         }
-
-        // Activate the new instrument
-        if (newIndex < instruments.Count)
+        else
         {
-            instruments[newIndex].SetActive(true);
-            currentInstrumentIndex = newIndex;
+            // Deactivate Tractor Beam
+            tractorBeamInstance.DeactivateTractorBeam();
+            Destroy(tractorBeamInstance.gameObject);
+            tractorBeamInstance = null;
         }
     }
 }
