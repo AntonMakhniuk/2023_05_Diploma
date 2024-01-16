81c04ed38128570899f42a053068a6a7e5aa4d9f
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-01-16 08:43:39 +0100

    feat: add airlock jam challenge

---
+++ Assets/Scripts/Production/Challenges/General/AirlockJam/AirlockButton.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Production.Challenges.General.AirlockJam
+{
+    public class AirlockButton
+    {
+        // TODO: Link the necessary visuals to the script
+        
+        public bool IsTurnedOn;
+
+        // The methods are split into normal and silent so that programmatic
+        // disabling of the buttons can be done without playing a sound or an
+        // animation to the player, to avoid overloading the senses
+        
+        public void TurnOff()
+        {
+            // TODO: Implement animations and sound
+            
+            TurnOffSilently();
+        }
+        
+        public void TurnOffSilently()
+        {
+            // TODO: Implement visual turning off
+            
+            IsTurnedOn = false;
+        }
+
+        public void TurnOn()
+        {
+            // TODO: Implement animations and sound
+            
+            TurnOnSilently();
+        }
+
+        public void TurnOnSilently()
+        {
+            // TODO: Implement visual turning on
+
+            IsTurnedOn = true;
+        }
+        
+        public IEnumerator Blink(float timePerBlink, int totalBlinkCount)
+        {
+            for (int blinkCount = 0; blinkCount < totalBlinkCount; blinkCount++)
+            {
+                float currentTime = 0f;
+
+                while (currentTime < timePerBlink)
+                {
+                    if (currentTime < timePerBlink / 3 && IsTurnedOn == true)
+                    {
+                        TurnOffSilently();
+                    }
+                    else if (currentTime >= timePerBlink / 3 && 
+                             currentTime < timePerBlink / 3 * 2 && 
+                             IsTurnedOn == false)
+                    {
+                        TurnOnSilently();
+                    }
+                    else if (currentTime < timePerBlink / 3 && IsTurnedOn == true)
+                    {
+                        TurnOffSilently();
+                    }
+                    
+                    currentTime += Time.deltaTime;
+                }
+            }
+            
+            TurnOnSilently();
+            
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Production/Challenges/General/AirlockJam/GenAirlockJam.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Production.Challenges.General.AirlockJam
+{
+    public class GenAirlockJam : GeneralBase<AirlockJamConfig>
+    {
+        public float timePerBlink;
+        [FormerlySerializedAs("blinkCount")] public int totalBlinkCount;
+        
+        private AirlockButton[] _buttons;
+        private int _turnedOffButtonsCount;
+        private bool _isBeingReset;
+        
+        public delegate void AirlockJamWarningHandler();
+        public event AirlockJamWarningHandler AirlockJamWarningSurpassed;
+        
+        protected override void UpdateChallenge()
+        {
+            if (_isBeingReset)
+            {
+                return;
+            }
+            
+            _turnedOffButtonsCount = 0;
+            
+            foreach (var airlockButton in _buttons)
+            {
+                if (airlockButton.IsTurnedOn == false)
+                {
+                    _turnedOffButtonsCount++;
+                }
+            }
+
+            if (_turnedOffButtonsCount >= Config.failThreshold)
+            {
+                Fail();
+            }
+            else if (_turnedOffButtonsCount >= Config.warningThreshold)
+            {
+                AirlockJamWarningSurpassed?.Invoke();
+            }
+        }
+
+        protected override void Reset()
+        {
+            if (_isBeingReset)
+            {
+                return;
+            }
+
+            StartCoroutine(ResetButtons());
+        }
+
+        private IEnumerator ResetButtons()
+        {
+            _isBeingReset = true;
+
+            foreach (var airlockButton in _buttons)
+            {
+                StartCoroutine(airlockButton.Blink(timePerBlink, totalBlinkCount));
+            }
+            
+            yield return new WaitForSeconds(timePerBlink * totalBlinkCount);
+            
+            _isBeingReset = false;
+
+            yield return null;
+        }
+    }
+
+    [Serializable]
+    public class AirlockJamConfig : ConfigBase
+    {
+        public int warningThreshold = 6;
+        public int failThreshold = 12;
+    }
+}
