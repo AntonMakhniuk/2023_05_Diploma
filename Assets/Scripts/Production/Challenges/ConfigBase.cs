aa4aed31c1f05bc577d74703ef93f90e6587f2f0
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-01-18 05:29:28 +0100

    feat: rework base classes

--- Assets/Scripts/Production/Challenges/ConfigBase.cs
+++ Assets/Scripts/Production/Challenges/ConfigBase.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Production.Challenges
 {
     [Serializable]
-    public abstract class ConfigBase
+    public abstract class ConfigBase : MonoBehaviour
     {
         public Difficulty difficulty;
+        public float resetWaitingTime;
     }
 }
--- Assets/Scripts/Production/Challenges/General/GeneralBase.cs
+++ Assets/Scripts/Production/Challenges/General/GeneralBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
@@ -7,53 +8,137 @@ namespace Production.Challenges.General
     public abstract class GeneralBase<TConfig> : MonoBehaviour, IGeneralChallenge where TConfig : ConfigBase
     {
         [SerializeField] protected TConfig[] configs;
+
+        private ProductionSessionManager _sessionManager;
+        private Difficulty _difficulty;
         
-        protected ProductionSessionManager SessionManager;
-        protected TConfig Config;
+        public TConfig Config { get; private set; }
 
         protected virtual void Start()
         {
-            SessionManager = GetComponentInParent<ProductionSessionManager>();
-            Config = configs.FirstOrDefault(config => config.difficulty == SessionManager.difficulty);
+            _sessionManager = GetComponentInParent<ProductionSessionManager>();
+
+            if (_sessionManager == null)
+            {
+                throw new Exception($"'{nameof(_sessionManager)}' is null. " +
+                                    $"{GetType().Name} has been instantiated outside ProductionSessionManager");
+            }
+            
+            // TODO: Change the difficulty to be given by the session manager instead of taken from it
+
+            _difficulty = _sessionManager.difficulty;
+            Config = configs.FirstOrDefault(config => config.difficulty == _difficulty);
+
+            if (Config == null)
+            {
+                Debug.LogError($"No appropriate config found for {GetType().Name} at {_difficulty}. " +
+                          "Reverting to first available config.");
+                
+                Config = configs[0];
+            }
+
+            if (Config == null)
+            {
+                throw new Exception($"No config available for {GetType().Name}. Cannot start challenge.");
+            }
         }
 
-        protected virtual void FixedUpdate()
+        private void FixedUpdate()
         {
+            HandlePerformanceConditions();
             UpdateChallenge();
         }
 
-        protected abstract void UpdateChallenge(); 
+        private bool _isBeingReset;
+        private bool _warningConditionsMet;
+        private bool _failConditionsMet;
+        private bool _isWarning;
+        
+        private void HandlePerformanceConditions()
+        {
+            if (_isBeingReset)
+            {
+                return;
+            }
+            
+            _warningConditionsMet = CheckWarningConditions();
+            _failConditionsMet = CheckFailConditions();
+
+            if (_failConditionsMet)
+            {
+                Fail();
+            }
+            else if (!_isWarning && _warningConditionsMet)
+            {
+                StartWarning();
+            }
+            else if (_isWarning && !_warningConditionsMet)
+            {
+                StopWarning();
+            }
+        }
+
+        protected abstract void UpdateChallenge();
+        
+        protected abstract bool CheckWarningConditions();
+        
+        protected abstract bool CheckFailConditions();
+
+        protected virtual void StartWarning()
+        {
+            _isWarning = true;
+        }
+
+        protected virtual void StopWarning()
+        {
+            _isWarning = false;
+        }
      
         public delegate void GeneralFailHandler();
-        public event GeneralFailHandler GeneralFailed;
+        public event GeneralFailHandler OnGeneralFail;
+        
         
         protected void Fail()
         {
-            GeneralFailed?.Invoke();
-            Reset();
-        }
-
-        protected abstract void Reset();
-
-        private GeneralFailHandler CreateFailHandler(Action onFailMethod)
-        {
-            return () => onFailMethod.Invoke();
+            OnGeneralFail?.Invoke();
+            
+            StartCoroutine(ResetCoroutine());
         }
         
-        public void AddMethodExecutedOnCriticalFail(Action onFailMethod)
+        private IEnumerator ResetCoroutine()
         {
-            GeneralFailed += CreateFailHandler(onFailMethod);
+            _isBeingReset = true;
+
+            HandleResetLogic();
+
+            yield return new WaitForSeconds(Config.resetWaitingTime);
+            
+            _isBeingReset = false;
+
+            yield return null;
         }
 
-        public void RemoveMethodExecutedOnCriticalFail(Action onFailMethod)
+        protected abstract void HandleResetLogic();
+        
+        private static GeneralFailHandler CreateFailHandlerDelegate(Action onFailMethod)
         {
-            GeneralFailed -= CreateFailHandler(onFailMethod);
+            return onFailMethod.Invoke;
+        }
+        
+        public void SubscribeToOnGeneralFail(Action onFailMethod)
+        {
+            OnGeneralFail += CreateFailHandlerDelegate(onFailMethod);
+        }
+
+        public void UnsubscribeFromOnGeneralFail(Action onFailMethod)
+        {
+            OnGeneralFail -= CreateFailHandlerDelegate(onFailMethod);
         }
     }
 
     public interface IGeneralChallenge
     {
-        void AddMethodExecutedOnCriticalFail(Action onFailMethod);
-        void RemoveMethodExecutedOnCriticalFail(Action onFailMethod);
+        void SubscribeToOnGeneralFail(Action onFailMethod);
+        void UnsubscribeFromOnGeneralFail(Action onFailMethod);
     }
 }
