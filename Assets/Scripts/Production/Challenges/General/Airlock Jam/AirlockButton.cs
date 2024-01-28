af1a0e5a097be7099e3d10fa1fe17706d9ceccf5
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-01-28 05:47:17 +0100

    feat: implement visuals for airlock jam challenge

--- Assets/Scripts/Production/Challenges/General/Airlock Jam/AirlockButton.cs
+++ Assets/Scripts/Production/Challenges/General/Airlock Jam/AirlockButton.cs
@@ -1,44 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace Production.Challenges.General.AirlockJam
+namespace Production.Challenges.General.Airlock_Jam
 {
     public class AirlockButton : MonoBehaviour
     {
+        // TODO: Change the visuals to more than just the color 
+
+        [SerializeField] private Image assignedImage;
+        
         // TODO: Link the necessary visuals to the script
         
-        public bool isTurnedOn;
+        public bool isTurnedOn = true;
+
+        private GenAirlockJam _airlockJam;
+
+        private void Start()
+        {
+            _airlockJam = GetComponentInParent<GenAirlockJam>();
+
+            if (_airlockJam == null)
+            {
+                throw new Exception($"'{nameof(_airlockJam)}' is null. " +
+                                    $"{GetType().Name} has been instantiated outside GenAirlockJam");
+            }
+        }
 
         // The methods are split into normal and silent so that programmatic
         // disabling of the buttons can be done without playing a sound or an
         // animation to the player, to avoid overloading the senses
+
+        public delegate void ButtonStatusHandler(AirlockButton button);
+        
+        public event ButtonStatusHandler OnButtonTurnedOff;
+
+        public void ChangeState()
+        {
+            switch (isTurnedOn)
+            {
+                case true:
+                {
+                    TurnOff();
+                    
+                    break;
+                }
+                case false:
+                {
+                    TurnOn();
+
+                    break;
+                }
+            }
+        }
         
         public void TurnOff()
         {
             // TODO: Implement animations and sound
             
+            if (!isTurnedOn)
+            {
+                return;
+            }
+            
             TurnOffSilently();
+            
+            OnButtonTurnedOff?.Invoke(this);
         }
         
-        private void TurnOffSilently()
+        public void TurnOffSilently()
         {
             // TODO: Implement visual turning off
             
             isTurnedOn = false;
+            
+            assignedImage.color = Color.red;
         }
 
+        public event ButtonStatusHandler OnButtonTurnedOn;
+        
         public void TurnOn()
         {
             // TODO: Implement animations and sound
+
+            if (isTurnedOn)
+            {
+                return;
+            }
             
             TurnOnSilently();
+            
+            OnButtonTurnedOn?.Invoke(this);
         }
 
-        private void TurnOnSilently()
+        public void TurnOnSilently()
         {
             // TODO: Implement visual turning on
 
             isTurnedOn = true;
+            
+            assignedImage.color = Color.white;
         }
         
         public IEnumerator Blink(float timePerBlink, int totalBlinkCount)
--- Assets/Scripts/Production/Challenges/General/Airlock Jam/GenAirlockJam.cs
+++ Assets/Scripts/Production/Challenges/General/Airlock Jam/GenAirlockJam.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-namespace Production.Challenges.General.AirlockJam
+namespace Production.Challenges.General.Airlock_Jam
 {
     public class GenAirlockJam : GeneralBase<AirlockJamConfig>
     {
-        private AirlockButton[] _allButtons;
+        [SerializeField] private GridLayoutGroup buttonGrid;
+        [SerializeField] private GameObject buttonPrefab;
+        
+        private List<AirlockButton> _allButtons;
         private List<AirlockButton> _enabledButtons;
         private List<AirlockButton> _disabledButtons;
+        private bool _disablingIsOnCooldown;
         
         // TODO: Add instantiation of buttons
 
@@ -18,8 +23,68 @@ namespace Production.Challenges.General.AirlockJam
         {
             base.Start();
 
-            _enabledButtons = new List<AirlockButton>(_allButtons);
+            _allButtons = new List<AirlockButton>();
             _disabledButtons = new List<AirlockButton>();
+            
+            InstantiateButtons();
+
+            foreach (var airlockButton in _allButtons)
+            {
+                airlockButton.OnButtonTurnedOff += ChangeButtonStatusToOff;
+                airlockButton.OnButtonTurnedOn += ChangeButtonStatusToOn;
+            }
+            
+            _enabledButtons = new List<AirlockButton>(_allButtons);
+
+            // Added so that buttons don't get disabled as soon as the challenge starts
+            StartCoroutine(DelayDisablingStart(Config.disableCooldown));
+        }
+        
+        private void ChangeButtonStatusToOff(AirlockButton button)
+        {
+            _enabledButtons.Remove(button);
+            _disabledButtons.Add(button);
+        }
+
+        private void ChangeButtonStatusToOn(AirlockButton button)
+        {
+            _disabledButtons.Remove(button);
+            _enabledButtons.Add(button);
+        }
+        
+        private IEnumerator DelayDisablingStart(float timeInSeconds)
+        {
+            _disablingIsOnCooldown = true;
+            
+            yield return new WaitForSeconds(timeInSeconds);
+
+            _disablingIsOnCooldown = false;
+
+            yield return null;
+        }
+        
+        // TODO: restructure so that this component is not responsible for both the logic and buttons?
+        
+        private void InstantiateButtons()
+        {
+            buttonGrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            buttonGrid.constraintCount = Config.gridSize;
+            
+            Vector2 cellSpacing = buttonGrid.spacing;
+            Rect parentRect = buttonGrid.transform.GetComponent<RectTransform>().rect;
+            RectOffset gridPadding = buttonGrid.padding;
+            
+            float targetCellWidth = (parentRect.width - gridPadding.horizontal) / Config.gridSize - cellSpacing.x;
+            float targetCellHeight = (parentRect.height - gridPadding.vertical) / Config.gridSize - cellSpacing.y;
+
+            buttonGrid.cellSize = new Vector2(targetCellWidth, targetCellHeight);
+
+            for (int row = 0; row < Config.gridSize * Config.gridSize; row++)
+            {
+                var newButton = Instantiate(buttonPrefab, buttonGrid.transform);
+
+                _allButtons.Add(newButton.GetComponent<AirlockButton>());
+            }
         }
 
         protected override bool CheckWarningConditions()
@@ -31,8 +96,6 @@ namespace Production.Challenges.General.AirlockJam
         {
             return _disabledButtons.Count >= Config.failThreshold;
         }
-        
-        private bool _disablingIsOnCooldown;
         
         protected override void HandleUpdateLogic()
         {
@@ -52,7 +115,7 @@ namespace Production.Challenges.General.AirlockJam
             for (int i = 0; i < numberOfDisabledButtons; i++)
             {
                 var chosenButton = _enabledButtons[Random.Range(0, _enabledButtons.Count)];
-                chosenButton.TurnOff();
+                chosenButton.TurnOffSilently();
                 _enabledButtons.Remove(chosenButton);
                 _disabledButtons.Add(chosenButton);
             }
@@ -92,6 +155,7 @@ namespace Production.Challenges.General.AirlockJam
     [Serializable]
     public class AirlockJamConfig : GeneralConfigBase
     {
+        public int gridSize = 6;
         public int totalBlinkCount = 3;
         public float singleBlinkTime = 0.5f;
         public int minDisabledButtonsPerTurn = 2;
