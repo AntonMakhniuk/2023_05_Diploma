14bc25f97afb6a8b1f979439de0c5c0f3a7a6408
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-01-29 05:29:58 +0100

    feat: add custom gradient background generation to temperature challenge

--- Assets/Scripts/Production/Challenges/General/Temperature/TemperatureSliderCommunicator.cs
+++ Assets/Scripts/Production/Challenges/General/Temperature/TemperatureSliderCommunicator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using ThirdParty.Scripts;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Production.Challenges.General.Temperature
@@ -7,7 +9,12 @@ namespace Production.Challenges.General.Temperature
     {
         public GenTemperature associatedChallenge;
         public Slider associatedSlider;
+        public Image sliderBackground;
 
+        public Color32 dangerZoneColor;
+        public Color32 warningZoneColor;
+        public Color32 safeZoneColor;
+        
         private TemperatureConfig _config;
 
         private void Start()
@@ -17,6 +24,21 @@ namespace Production.Challenges.General.Temperature
             associatedSlider.maxValue = _config.failThreshold;
             associatedSlider.minValue = _config.minTemperature;
             associatedSlider.value = _config.baseTemperature;
+
+            List<CustomGradientKey> colorKeys = new List<CustomGradientKey>();
+            
+            colorKeys.Add(new CustomGradientKey(safeZoneColor, 0f));
+            colorKeys.Add(new CustomGradientKey(safeZoneColor, 
+                (float)_config.warningThreshold / _config.maxTemperature));
+            colorKeys.Add(new CustomGradientKey(warningZoneColor, 
+                (float)_config.warningThreshold / _config.maxTemperature + 0.001f));
+            colorKeys.Add(new CustomGradientKey(warningZoneColor,
+                (float)_config.failThreshold / _config.maxTemperature));
+            colorKeys.Add(new CustomGradientKey(dangerZoneColor,
+                (float)_config.failThreshold / _config.maxTemperature + 0.001f));
+            colorKeys.Add(new CustomGradientKey(dangerZoneColor, 1f));
+            
+            Utility.CreateGradientSpriteAndApplyToSlider(associatedSlider, sliderBackground, colorKeys.ToArray());
         }
 
         private void FixedUpdate()
