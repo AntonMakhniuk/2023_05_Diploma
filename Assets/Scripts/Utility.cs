515829a5952c4782a95aa9b47fbbd1ea3db1098b
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-01-29 05:28:51 +0100

    feat: introduce custom gradient class and background creator method

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,4 +1,8 @@
-﻿public abstract class Utility
+﻿using ThirdParty.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Utility
 {
     public static T GetRandomEnum<T>()
     {
@@ -6,5 +10,44 @@
         T v = (T)a.GetValue(UnityEngine.Random.Range(0,a.Length));
             
         return v;
+    }
+
+    public static void CreateGradientSpriteAndApplyToSlider(Slider slider, Image backgroundImage,
+        CustomGradientKey[] colorKeys)
+    {
+        var sliderRect = slider.GetComponent<RectTransform>().rect;
+        
+        var gradient = new CustomGradient();
+
+        foreach (var colorKey in colorKeys)
+        {
+            gradient.AddKey(colorKey);
+        }
+
+        Texture2D texture2D = new Texture2D((int) sliderRect.width, (int)sliderRect.height)
+        {
+            wrapMode = TextureWrapMode.Clamp
+        };
+
+        for (int x = 0; x < sliderRect.width; x++)
+        {
+            float t = Mathf.InverseLerp(0, sliderRect.width - 1, x);
+
+            for (int y = 0; y < sliderRect.height; y++)
+            {
+                texture2D.SetPixel(x, y, gradient.Evaluate(t));
+            }
+        }
+        
+        texture2D.Apply();
+
+        Sprite sprite = Sprite.Create
+        (
+            texture2D, 
+            new Rect(0, 0, sliderRect.width, sliderRect.height), 
+            Vector2.one * 0.5f
+        );
+
+        backgroundImage.sprite = sprite;
     }
 }
---
+++ Assets/ThirdParty/Scripts/CustomGradient.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+  // Author : orionsyndrome
+  // Obtained from: https://forum.unity.com/threads/gradients-number-of-keys-and-alpha-values.1231029/
+
+namespace ThirdParty.Scripts
+{
+    public struct CustomGradientKey {
+     
+      public float T { get; set; }
+      public Color Color { get; set; } // comes with r, g, b, alpha
+     
+      public CustomGradientKey(Color color, float t) {
+        Color = color;
+        T = t;
+      }
+     
+    }
+     
+    public class CustomGradient {
+     
+      private List<CustomGradientKey> _keys = new();
+
+      public int Count => _keys.Count;
+     
+      public CustomGradientKey this[int index] {
+        get => _keys[index];
+        set { _keys[index] = value; SortKeys(); }
+      }
+     
+      public void AddKey(Color color, float t)
+        => AddKey(new CustomGradientKey(color, t));
+     
+      public void AddKey(CustomGradientKey key) {
+        _keys.Add(key);
+        SortKeys();
+      }
+     
+      public void InsertKey(int index, Color color,  float t)
+        => InsertKey(index, new CustomGradientKey(color, t));
+     
+      public void InsertKey(int index, CustomGradientKey key) {
+        _keys.Insert(index, key);
+        SortKeys();
+      }
+     
+      public void RemoveKey(int index) {
+        _keys.RemoveAt(index);
+        SortKeys();
+      }
+     
+      public void RemoveInRange(float min, float max) {
+        for(int i = _keys.Count - 1; i >= 0; i--)
+          if(_keys[i].T >= min && _keys[i].T <= max) _keys.RemoveAt(i);
+        SortKeys();
+      }
+     
+      public void Clear() => _keys.Clear();
+     
+      private void SortKeys() => _keys.Sort( (a, b) => a.T.CompareTo(b.T) );
+     
+      private (int l, int r) GetNeighborKeys(float t) {
+        var l = Count - 1;
+     
+        for(int i = 0; i <= l; i++) {
+          if(_keys[i].T >= t) {
+            if(i == 0) return ( -1, i );
+            return ( i - 1, i );
+          }
+        }
+     
+        return ( l, -1 );
+      }
+     
+      public Color Evaluate(float t) {
+        if(Count == 0) return new Color(0f, 0f, 0f, 0f);
+     
+        var n = GetNeighborKeys(t);
+     
+        if(n.l < 0) return _keys[n.r].Color;
+          else if(n.r < 0) return _keys[n.l].Color;
+     
+        return Color.Lerp(
+          _keys[n.l].Color,
+          _keys[n.r].Color,
+          Mathf.InverseLerp(_keys[n.l].T, _keys[n.r].T, t)
+        );
+      }
+     
+    }
+
+}
