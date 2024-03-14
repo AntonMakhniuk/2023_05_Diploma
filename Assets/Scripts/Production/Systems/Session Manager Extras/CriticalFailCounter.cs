388e3c734d4320d380b76d5994df42f12d004ad3
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-03-14 18:55:16 +0100

    feat: add extra information displays for Production

---
+++ Assets/Scripts/Production/Systems/Session Manager Extras/CriticalFailCounter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+namespace Production.Systems.Session_Manager_Extras
+{
+    public class CriticalFailCounter : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] errorDisplays;
+
+        private ProductionSessionManager _sessionManager;
+
+        private void Start()
+        {
+            _sessionManager = GetComponentInParent<ProductionSessionManager>();
+
+            if (_sessionManager == null)
+            {
+                throw new Exception($"'{nameof(_sessionManager)}' is null. " +
+                                    $"{GetType().Name} has been instantiated outside ProductionSessionManager");
+            }
+
+            _sessionManager.OnCriticalFailReachedManager += ActivateNextErrorDisplay;
+        }
+
+        private void ActivateNextErrorDisplay(object sender, int criticalFailCount)
+        {
+            if (criticalFailCount > errorDisplays.Length)
+            {
+                Debug.LogError("More critical errors sent than there are available error displays.");
+                
+                return;
+            }
+            
+            // the subtraction accounts for the indexing starting from 0 and not 1
+            var imageToChange = errorDisplays[criticalFailCount - 1].GetComponent<Image>();
+
+            Color.RGBToHSV(imageToChange.color, out var h, out var s, out var _);
+            
+            imageToChange.color = Color.HSVToRGB(h, s, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Production/Systems/Session Manager Extras/ProductionSessionTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Production.Systems.Session_Manager_Extras
+{
+    public class ProductionSessionTimer : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private float updateRate = 0.0333f;
+        
+        private ProductionSessionManager _sessionManager;
+        private float _currentTimeSeconds;
+        private TimeSpan _currentTimeSpan;
+        
+        private void Start()
+        {
+            _sessionManager = GetComponentInParent<ProductionSessionManager>();
+
+            if (_sessionManager == null)
+            {
+                throw new Exception($"'{nameof(_sessionManager)}' is null. " +
+                                    $"{GetType().Name} has been instantiated outside ProductionSessionManager");
+            }
+
+            _currentTimeSeconds = _sessionManager.CraftingData.Recipe.difficultyConfig.productionLengthInSeconds;
+            
+            InvokeRepeating(nameof(UpdateTimer), 0, updateRate);
+        }
+
+        private void UpdateTimer()
+        {
+            _currentTimeSeconds -= updateRate;
+
+            if (_currentTimeSeconds <= 0)
+            {
+                StopProductionInManager();
+            }
+
+            _currentTimeSpan = TimeSpan.FromSeconds(_currentTimeSeconds);
+            timerText.text = _currentTimeSpan.ToString("mm\\:ss");
+        }
+        
+        private void StopProductionInManager()
+        {
+            _sessionManager.FinishProductionSuccessfully();
+        }
+    }
+}
