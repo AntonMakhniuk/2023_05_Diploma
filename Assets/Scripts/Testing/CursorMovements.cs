d024e4e6d06715b4a248c0fa39feae26e81f9f9c
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2024-10-19 11:46:25 +0200

    Fix: Fixed drone rotation

--- Assets/Scripts/Testing/CursorMovements.cs
+++ Assets/Scripts/Testing/CursorMovements.cs
@@ -1,3 +1,4 @@
+using System;
 using Player;
 using UnityEngine;
 
@@ -7,15 +8,24 @@ namespace Testing
     {
         [SerializeField] private float followSmoothing = 0.1f;
         [SerializeField] private float cursorMovementScale = 0.1f;
+        [SerializeField] private float distanceToShip = 20f;
+        [SerializeField] private Transform droneSetupCenter;
+        
 
-        private Vector3 currentVelocity = Vector3.zero;
+        private Vector3 _currentVelocity = Vector3.zero;
         private PlayerInputActions _playerInputActions;
-        private Vector3 targetPosition;
+        private Vector3 _targetPosition;
+        private Vector3 _posCenteredUnscaled;
+        private Transform _mainCam;
+       
+
 
         private void Awake()
         {
             _playerInputActions = PlayerActions.InputActions;
             _playerInputActions.PlayerCamera.Enable();
+
+            _mainCam = Camera.main.transform;
         }
 
         private void OnEnable()
@@ -32,19 +42,23 @@ namespace Testing
         {
             Vector2 mouseDelta = _playerInputActions.PlayerCamera.CameraMovement.ReadValue<Vector2>();
             
-            Camera cam = Camera.main;
-            
-            Vector3 rightMovement = cam.transform.right * mouseDelta.x;
-            Vector3 forwardMovement = cam.transform.up * mouseDelta.y;
+            Vector3 rightMovement = _mainCam.right * mouseDelta.x;
+            Vector3 forwardMovement = _mainCam.up * mouseDelta.y;
             
             Vector3 deltaWorld = (rightMovement + forwardMovement) * cursorMovementScale;
+
+            _posCenteredUnscaled = ((_targetPosition + deltaWorld) - droneSetupCenter.position);
             
-            targetPosition += deltaWorld;
+            _targetPosition = distanceToShip / Mathf.Sqrt(
+                Mathf.Pow(_posCenteredUnscaled.x, 2) +  
+                Mathf.Pow(_posCenteredUnscaled.y, 2) + 
+                Mathf.Pow(_posCenteredUnscaled.z, 2)) * _posCenteredUnscaled + droneSetupCenter.position;
         }
 
         private void FixedUpdate()
         {
-            Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followSmoothing);
+            
+            Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, _targetPosition, ref _currentVelocity, followSmoothing);
             transform.position = smoothPosition;
         }
     }
--- Assets/Scripts/Testing/DroneMovements.cs
+++ Assets/Scripts/Testing/DroneMovements.cs
@@ -6,14 +6,14 @@ namespace Testing
     [RequireComponent(typeof(Rigidbody))]
     public class DroneMovements : MonoBehaviour
     {
-        public static DroneMovements Instance;
-        
+         public static DroneMovements Instance;
+
         [SerializeField] private float thrustSpeed = 50f;
         [SerializeField] private float boostMultiplier = 2f;
-        
-        
+        [SerializeField] private Transform lookTarget; 
+        [SerializeField] private float rotationSpeed = 2f;
+
         private Rigidbody _rb;
-        
         private PlayerInputActions _playerInputActions;
 
         private void Awake()
@@ -43,10 +43,15 @@ namespace Testing
             {
                 Move(new Vector2(0, _playerInputActions.PlayerShip.Yaw.ReadValue<float>()));
             }
+
+            
+            Vector3 directionToTarget = (lookTarget.position - transform.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
+            _rb.rotation = Quaternion.Slerp(_rb.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            
         }
 
-
-        private void Move(Vector2 movementVector) 
+        private void Move(Vector2 movementVector)
         {
             _rb.AddRelativeForce(thrustSpeed * boostMultiplier * Time.deltaTime 
                                  * new Vector3(movementVector.y, 0, movementVector.x), ForceMode.Force);
