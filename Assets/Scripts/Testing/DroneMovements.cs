1638bad4e5455382fda59eb1ef35a19db26997fb
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2024-10-20 18:24:54 +0200

    Fix: Fixed Movements

--- Assets/Scripts/Testing/DroneMovements.cs
+++ Assets/Scripts/Testing/DroneMovements.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 
 namespace Testing
 {
-    [RequireComponent(typeof(Rigidbody))]
+    
     public class DroneMovements : MonoBehaviour
     {
          public static DroneMovements Instance;
 
         [SerializeField] private float thrustSpeed = 50f;
+        [SerializeField] private float rotationSpeed = 2f;
         [SerializeField] private float boostMultiplier = 2f;
         [SerializeField] private Transform lookTarget; 
-        [SerializeField] private float rotationSpeed = 2f;
+        
 
-        private Rigidbody _rb;
+        
         private PlayerInputActions _playerInputActions;
 
         private void Awake()
@@ -29,7 +30,7 @@ namespace Testing
             }
 
             _playerInputActions = PlayerActions.InputActions;
-            _rb = GetComponent<Rigidbody>();
+            
             _playerInputActions.PlayerShip.Enable();
         }
 
@@ -37,25 +38,47 @@ namespace Testing
         {
             if (_playerInputActions.PlayerShip.Thrust.IsPressed())
             {
-                Move(new Vector2(_playerInputActions.PlayerShip.Thrust.ReadValue<float>(), 0));
+                float thrustInput = _playerInputActions.PlayerShip.Thrust.ReadValue<float>();
+                MoveThrust(thrustInput);
             }
+
             if (_playerInputActions.PlayerShip.Yaw.IsPressed())
             {
-                Move(new Vector2(0, _playerInputActions.PlayerShip.Yaw.ReadValue<float>()));
+                float yawInput = _playerInputActions.PlayerShip.Yaw.ReadValue<float>();
+                RotateYaw(yawInput);
             }
 
             
             Vector3 directionToTarget = (lookTarget.position - transform.position).normalized;
             Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
-            _rb.rotation = Quaternion.Slerp(_rb.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             
         }
 
-        private void Move(Vector2 movementVector)
+        private void MoveThrust(float thurstInput)
         {
-            _rb.AddRelativeForce(thrustSpeed * boostMultiplier * Time.deltaTime 
-                                 * new Vector3(movementVector.y, 0, movementVector.x), ForceMode.Force);
+            if (thurstInput != 0)
+            {
+                float currentSpeed = thrustSpeed * (thurstInput > 0 ? boostMultiplier : 1f);
+                transform.Translate( thurstInput * currentSpeed * Time.deltaTime * Vector3.forward);
+            }
         }
+        private void RotateYaw(float yawInput)
+        {
+            if (yawInput != 0)
+            {
+                Vector3 directionToTarget = transform.position - lookTarget.position;
+
+                float yawRotation = yawInput * rotationSpeed * Time.deltaTime;
+
+                directionToTarget = Quaternion.Euler(0, yawRotation, 0) * directionToTarget;
+
+                transform.position = lookTarget.position + directionToTarget;
+
+                transform.LookAt(lookTarget);
+            }
+        }
+
 
     }
 
