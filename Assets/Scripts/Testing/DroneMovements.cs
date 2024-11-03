0d7dec5c94392cf95a9ff807e42360a19f8967f9
alekskorik <skorikalesha@gmail.com>
2024-11-03 18:42:32 +0100

    Dodge
    Possibility of dodge added

--- Assets/Scripts/Testing/DroneMovements.cs
+++ Assets/Scripts/Testing/DroneMovements.cs
@@ -7,14 +7,14 @@ namespace Testing
     public class DroneMovements : MonoBehaviour
     {
         public static DroneMovements Instance;
-        
+
         [SerializeField] private float thrustSpeed = 50f;
         [SerializeField] private float strafeSpeed = 30f;
         [SerializeField] private float boostMultiplier = 2f;
-        
-        
+        [SerializeField] private float dodgeSpeed = 5f; 
+        private bool isDodging = false; 
+
         private Rigidbody _rb;
-        
         private PlayerInputActions _playerInputActions;
 
         private void Awake()
@@ -36,6 +36,7 @@ namespace Testing
 
         private void FixedUpdate()
         {
+            
             if (_playerInputActions.PlayerShip.Thrust.IsPressed())
             {
                 Move(new Vector2(_playerInputActions.PlayerShip.Thrust.ReadValue<float>(), 0));
@@ -44,15 +45,38 @@ namespace Testing
             {
                 Move(new Vector2(0, _playerInputActions.PlayerShip.Yaw.ReadValue<float>()));
             }
+
+           
+            if (!isDodging && _playerInputActions.PlayerShip.Roll.IsPressed())
+            {
+                float dodgeDirection = _playerInputActions.PlayerShip.Roll.ReadValue<float>();
+                if (dodgeDirection > 0) 
+                {
+                    Dodge(Vector3.left);
+                }
+                else if (dodgeDirection < 0) 
+                {
+                    Dodge(Vector3.right);
+                }
+            }
         }
 
-
-        private void Move(Vector2 movementVector) 
+        private void Move(Vector2 movementVector)
         {
-            _rb.AddRelativeForce(thrustSpeed * boostMultiplier * Time.deltaTime 
+            _rb.AddRelativeForce(thrustSpeed * boostMultiplier * Time.deltaTime
                                  * new Vector3(movementVector.y, 0, movementVector.x), ForceMode.Force);
         }
 
-    }
+        private void Dodge(Vector3 direction)
+        {
+            isDodging = true;
+            _rb.AddRelativeForce(direction * dodgeSpeed, ForceMode.Impulse); 
+            Invoke(nameof(EndDodge), 0.1f); 
+        }
 
+        private void EndDodge()
+        {
+            isDodging = false;
+        }
+    }
 }
