b8635a8637f4ed862bf81d10ee7d358a428122dd
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2024-11-10 14:27:38 +0100

    feat: Added new movement system

--- Assets/Scripts/Tools/Miscellaneous/Crosshair.cs
+++ Assets/Scripts/Tools/Miscellaneous/Crosshair.cs
@@ -1,23 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class Crosshair : MonoBehaviour
 { 
     [SerializeField] private RectTransform crosshairUI;
-    [SerializeField] private float returnSpeed = 5f;
-    [SerializeField] private float smoothing = 0.1f;
-    
+    [SerializeField] public float maxRadius = 100f;
     private PlayerInputActions _playerInputActions;
-    private Vector2 targetPosition;
-    private Vector2 smoothPosition;
-    private Vector2 screenCenter;
 
     void Awake()
     {
         _playerInputActions = new PlayerInputActions();
-        screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
-        targetPosition = screenCenter;
-        smoothPosition = screenCenter;
     }
 
     void OnEnable()
@@ -30,26 +22,16 @@ public class Crosshair : MonoBehaviour
         _playerInputActions.PlayerCamera.CameraMovement.Disable();
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        Vector2 mouseDelta = _playerInputActions.PlayerCamera.CameraMovement.ReadValue<Vector2>();
-
-        if (mouseDelta != Vector2.zero)
-        {
-            targetPosition += mouseDelta;
-        }
-        else
-        {
-            targetPosition = Vector2.Lerp(targetPosition, screenCenter, Time.deltaTime * returnSpeed);
-        }
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
         
-        targetPosition.x = Mathf.Clamp(targetPosition.x, 0, Screen.width);
-        targetPosition.y = Mathf.Clamp(targetPosition.y, 0, Screen.height);
-        
-        smoothPosition = Vector2.Lerp(smoothPosition, targetPosition, smoothing);
-        
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(crosshairUI.parent as RectTransform, smoothPosition, null, out Vector2 localPoint))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(crosshairUI.parent as RectTransform, mousePosition, null, out Vector2 localPoint))
         {
+            if (localPoint.magnitude > maxRadius)
+            {
+                localPoint = localPoint.normalized * maxRadius;
+            }
             crosshairUI.localPosition = localPoint;
         }
     }
