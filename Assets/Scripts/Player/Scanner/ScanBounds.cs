099a6e61313f8be82015fde51ae3fd537b9da517
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-11-20 03:40:36 +0100

    feat: introduce new shader and scanner material

--- Assets/Scripts/Player/Scanner/ScanBounds.cs
+++ Assets/Scripts/Player/Scanner/ScanBounds.cs
@@ -4,6 +4,18 @@ namespace Player.Scanner
 {
     public class ScanBounds : MonoBehaviour
     {
+        private Quaternion _initialRotation;
+
+        private void Start()
+        {
+            _initialRotation = transform.rotation;
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = _initialRotation;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<Scannable>(out var scannable))
