253a87b672b8f8afc0acef8a24a4e8c6c3683e0d
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-12-04 01:19:05 +0100

    feat: update tool inventory to add marker

--- Assets/Scripts/Player/Ship/ToolInventory.cs
+++ Assets/Scripts/Player/Ship/ToolInventory.cs
@@ -1,34 +1,36 @@
 using System;
 using Systems.Mining.Tools.Base_Tools;
-using Testing;
-using Tools.Base_Tools;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 //TODO: Should be rewritten
 namespace Player.Ship
 {
-    public class Inventory : MonoBehaviour
+    public class ToolInventory : MonoBehaviour
     {
         [SerializeField] private BaseTool instrument1;
         [SerializeField] private BaseTool instrument2;
         [SerializeField] private BaseTool instrument3;
+        [SerializeField] private BaseTool marker;
         
         private BaseTool _activeBaseTool;
         
         private Action<InputAction.CallbackContext> _toggleInstrument1;
         private Action<InputAction.CallbackContext> _toggleInstrument2;
         private Action<InputAction.CallbackContext> _toggleInstrument3;
+        private Action<InputAction.CallbackContext> _toggleMarker;
 
         private void Start()
         {
             _toggleInstrument1 = _ => ToggleInstrument(instrument1);
             _toggleInstrument2 = _ => ToggleInstrument(instrument2);
             _toggleInstrument3 = _ => ToggleInstrument(instrument3);
+            _toggleMarker = _ => ToggleInstrument(marker);
 
             PlayerActions.InputActions.PlayerShip.ToggleSlot_1.performed += _toggleInstrument1;
             PlayerActions.InputActions.PlayerShip.ToggleSlot_2.performed += _toggleInstrument2;
             PlayerActions.InputActions.PlayerShip.ToggleSlot_3.performed += _toggleInstrument3;
+            PlayerActions.InputActions.PlayerShip.ToggleMarker.performed += _toggleMarker;
         }
 
         private void ToggleInstrument(BaseTool tool)
@@ -55,6 +57,7 @@ namespace Player.Ship
             PlayerActions.InputActions.PlayerShip.ToggleSlot_1.performed -= _toggleInstrument1;
             PlayerActions.InputActions.PlayerShip.ToggleSlot_2.performed -= _toggleInstrument2;
             PlayerActions.InputActions.PlayerShip.ToggleSlot_3.performed -= _toggleInstrument3;
+            PlayerActions.InputActions.PlayerShip.ToggleMarker.performed -= _toggleMarker;
         }
     }
 }
