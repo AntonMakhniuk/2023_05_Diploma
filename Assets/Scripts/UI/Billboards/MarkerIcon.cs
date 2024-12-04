7313330af03f4c2a5e9d2ab7adeba34916b1d9c0
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-12-04 06:34:03 +0100

    feat: introduce billboarding implementation

---
+++ Assets/Scripts/UI/Billboards/MarkerIcon.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using NaughtyAttributes;
+using Player.Ship.Tools.Marker;
+using TMPro;
+using UI.Systems.Miscellaneous;
+using UnityEngine;
+
+namespace UI.Billboards
+{
+    public class MarkerIcon : BaseBillboard
+    {
+        [Foldout("Visualisation Data")] [SerializeField]
+        private SpriteRenderer resourceSprite;
+        [Foldout("Visualisation Data")] [SerializeField]
+        private TMP_Text resourceNameText;
+        [Foldout("Visualisation Data")] [SerializeField]
+        private TMP_Text quantityText;
+        [Foldout("Visualisation Data")] [SerializeField]
+        private TMP_Text distanceText;
+
+        [Foldout("Resource Data")] [SerializeField]
+        private Collectable resourceData;
+
+        private void Start()
+        {
+            resourceSprite.sprite = resourceData.resource.icon;
+            resourceNameText.SetText(resourceData.resource.name);
+            quantityText.SetText(resourceData.AmountContained.ToString(CultureInfo.InvariantCulture));
+            distanceText.SetText(DistanceFromPlayer.ToString("N2") + "m");
+        }
+
+        protected override void LateUpdate()
+        {
+            base.LateUpdate();
+            
+            distanceText.SetText(DistanceFromPlayer.ToString("N2") + "m");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Systems/Miscellaneous/BaseBillboard.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NaughtyAttributes;
+using Testing.NewMovement;
+using UnityEngine;
+
+namespace UI.Systems.Miscellaneous
+{
+    public class BaseBillboard : MonoBehaviour
+    {
+        [Foldout("Billboard Data")] [SerializeField]
+        private float scaleMultiplier = 5f;
+        [Foldout("Billboard Data")] [SerializeField]
+        private float fadeStartDistance = 300f;
+        [Foldout("Billboard Data")] [SerializeField]
+        private float fadeEndDistance = 500f;
+        [Foldout("Billboard Data")] [SerializeField]
+        private float distanceScaleMultiplier = 5f;
+        [Foldout("Billboard Data")] [SerializeField]
+        private float scaleStartDistance = 100f;
+        [Foldout("Billboard Data")] [SerializeField]
+        private CanvasGroup canvasGroup;
+        
+        protected float DistanceFromPlayer;
+        
+        private Vector3 _originalScale;
+        private List<SpriteRenderer> _sprites;
+
+        private void Awake()
+        {
+            var parentScale = transform.parent != null ? transform.parent.lossyScale : Vector3.one;
+            var inverseParentScale = 
+                new Vector3(1f / parentScale.x, 1f / parentScale.y, 1f / parentScale.z);
+            
+            transform.localScale = Vector3.Scale(transform.localScale, inverseParentScale) * scaleMultiplier;
+            _originalScale = transform.localScale;
+            _sprites = GetComponentsInChildren<SpriteRenderer>().ToList();
+        }
+
+        protected virtual void LateUpdate()
+        {
+            transform.LookAt(Camera.main!.transform.position, Camera.main!.transform.up);
+            
+            DistanceFromPlayer =
+                Vector3.Distance(transform.position, DroneController.Instance.gameObject.transform.position);
+
+            if (DistanceFromPlayer >= fadeEndDistance)
+            {
+                canvasGroup.alpha = 0f;
+
+                foreach (var sprite in _sprites)
+                {
+                    var color = sprite.color;
+                    color.a = 0f;
+                    sprite.color = color;
+                }
+            }
+            else if (DistanceFromPlayer >= fadeStartDistance)
+            {
+                canvasGroup.alpha = fadeEndDistance - DistanceFromPlayer / fadeEndDistance - fadeStartDistance;
+                
+                foreach (var sprite in _sprites)
+                {
+                    var color = sprite.color;
+                    color.a = fadeEndDistance - DistanceFromPlayer / fadeEndDistance - fadeStartDistance;
+                    sprite.color = color;
+                }
+            }
+            else
+            {
+                canvasGroup.alpha = 1f;
+                
+                foreach (var sprite in _sprites)
+                {
+                    var color = sprite.color;
+                    color.a = 1f;
+                    sprite.color = color;
+                }
+            }
+
+            var parentScale = transform.parent != null ? transform.parent.lossyScale : Vector3.one;
+            var inverseParentScale = new Vector3(1f / parentScale.x, 1f / parentScale.y, 1f / parentScale.z);
+            
+            Debug.Log("dist: " + DistanceFromPlayer);
+            
+            if (DistanceFromPlayer > scaleStartDistance)
+            {
+                var normalizedDistance = 
+                    Mathf.InverseLerp(scaleStartDistance, fadeEndDistance, DistanceFromPlayer);
+                Debug.Log("normDist: " + normalizedDistance);
+                
+                var scaleFactor = Mathf.Lerp(_originalScale.x, distanceScaleMultiplier, normalizedDistance);
+                Debug.Log("scale: " + scaleFactor);
+                
+                transform.localScale = Vector3.Scale(_originalScale * scaleFactor, inverseParentScale);
+            }
+            else
+            {
+                transform.localScale = Vector3.Scale(_originalScale, inverseParentScale);
+            }
+        }
+    }
+}
