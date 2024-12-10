50ed265080f05d29ee5e9864254c7f7e9700545e
Sarnai <32843738+FrozzLab@users.noreply.github.com>
2024-12-10 01:44:59 +0100

    feat: update drone controller implementation

---
+++ Assets/Scripts/Player/Movement/Drone Movement/EngineGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Player.Movement.Drone_Movement
+{
+    [Serializable]
+    public class EngineGroup : MonoBehaviour
+    {
+        public List<VisualEffect> engines = new();
+    }
+
+    public enum EngineGroupType
+    {
+        Top, Bottom, Left, Right, Front, Back
+    }
+}
